d1825da298c4d2b6b55f0105eb1cf660c24a9656
BudoBoy07 <BudoBoy07@gmail.com>
2023-03-08 15:53:03 +0100

    I'm suffering from analysis paralysis

---
+++ webscrape_files/business/collect_data/load_webpage/load_webpage.py
@@ -0,0 +1,11 @@
+class LoadWebpage:
+
+    def launch_selenium_webdriver():
+        pass
+
+    def access_url_with_selenium_webdriver():
+        pass
+
+    def return_page_source_as_string():
+        pass
+
