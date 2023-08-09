e9efdcf623c14364d1f56bb0c63b4280629eed83
BudoBoy07 <BudoBoy07@gmail.com>
2023-08-09 16:00:34 +0200

    I need to go out and live now

--- sandbox_3/choose_love.py
+++ sandbox_3/choose_love.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Dict, List, Type, Callable
+from typing import Dict, List, Tuple, Type, Callable
 
 
 class DAO(ABC):
@@ -7,19 +7,19 @@ class DAO(ABC):
     def exists(self, key: str) -> bool:
         return self._is_accessible(key)
 
-    def get(self, key: str) -> 'DataObject':
+    def get(self, key: str) -> 'Composite':
         self._raise_error_if_key_missing(key)
         return self._read(key)
 
-    def set_unique_key(self, key: str, value: 'DataObject') -> None:
+    def set_unique_key(self, key: str, value: 'Composite') -> None:
         self._raise_error_if_key_exists(key)
         self._set(key, value)
 
-    def set_if_key_missing(self, key: str, value: 'DataObject') -> None:
+    def set_if_key_missing(self, key: str, value: 'Composite') -> None:
         if not self.exists(key):
             self._set(key, value)
 
-    def _set(self, key: str, value: 'DataObject') -> None:
+    def _set(self, key: str, value: 'Composite') -> None:
         self._write(key, value)
 
     def _raise_error_if_key_exists(self, key: str) -> None:
@@ -36,42 +36,31 @@ class DAO(ABC):
         pass
 
     @abstractmethod
-    def _read(self, key: str) -> 'DataObject':
+    def _read(self, key: str) -> 'Composite':
         pass
 
     @abstractmethod
-    def _write(self, key: str, value: 'DataObject') -> None:
+    def _write(self, key: str, value: 'Composite') -> None:
         pass
 
 class Registry(DAO):
     def __init__(self) -> None:
-        self.dct: Dict[str, DataObject] = {}
+        self.dct: Dict[str, Composite] = {}
 
     def _is_accessible(self, key: str) -> bool:
         return key in self.dct
 
-    def _read(self, key: str) -> 'DataObject':
+    def _read(self, key: str) -> 'Composite':
         return self.dct[key]
 
-    def _write(self, key: str, value: 'DataObject') -> None:
+    def _write(self, key: str, value: 'Composite') -> None:
         self.dct[key] = value
 
-class FileAccess(DAO):
-
-    def _is_accessible(self, key: str) -> bool:
-        if key == "str":
-            return True
-        return False
-
-    def _read(self, key: str) -> 'DataObject':
-        assert key == "hey"
-        return School(key)
-
-    def _write(self, key: str, value: 'DataObject') -> None:
-        pass
-
 class DataStrategy:
-    pass
+
+    @staticmethod
+    def generate_data_object(time_period: 'TimePeriod', name: str) -> 'Composite':
+        return EndPoint(name, Domain("test"), time_period)
 
 class DomainStrategy(ABC):
 
@@ -161,186 +150,199 @@ class DtuStrategy(DomainStrategy):
     def time_strategy():
         return DataStrategy#DtuTerm
 
-
-class DataObject(ABC):
-
-    KEY_SEPARATOR = "__"
-    ROOT_NAME: str = "root"
-    registry: Registry = Registry()
+class Domain:
 
     def __init__(self, key) -> None:
         self.name = key
-        self.register()
+        self.strategy: DomainStrategy = DtuStrategy()
 
     def get_name(self) -> str:
         return self.name
 
-    def _get_class_name(self) -> str:
-        return self.__class__.__name__
+    def get_data_strategy(self, class_id: str) -> Type[DataStrategy]:
+        if class_id == Composite.CLASS_ID:
+            pass
+        return DataStrategy
 
-    def get_key_separator(self):
-        return DataObject.KEY_SEPARATOR
+class TimePeriod:
 
-    def register(self) -> None:
-        key = self.generate_general_key()
-        DataObject.registry.set_unique_key(key, self)
+    def __init__(self, key) -> None:
+        self.name = key
 
-    def generate_general_key(self) -> str:
-        separator: str = self.get_key_separator()
-        class_and_separator: str = self._get_class_name() + separator
-        key_modifier: str = self._key_modifier()
-        name: str = self.name
-        if self._key_modifier() != "":
-            return class_and_separator + key_modifier + separator + name
-        else:
-            return class_and_separator + name
+    def get_name(self) -> str:
+        return self.name
 
-    def _key_modifier(self) -> str:
-        return ""
+class Serializer:
 
-
-class Composite(DataObject):
-
-    KEY_SEPARATOR = "__"
-
-    def __init__(self, name: str) -> None:
-        self.children: List[Composite]
-        super().__init__(name)
-
-    def add_child(self, child: 'Composite') -> None:
-        self.children.append(child)
-
-    def get_children(self) -> List['Composite']:
-        return self.children
-
-    def perform_action(self, key: str, action: Callable[[str], str]) -> str:
-        result = action(key)
-        return result
-
-class Domain(Composite):
-
-    def get_data_strategy(self) -> Type[DomainStrategy]:
-        return DtuStrategy
-
-class DomainSpecificObject(Composite):
-
-    def __init__(self, name: str) -> None:
-        self.domain: Domain
-        super().__init__(name)
-
-    def get_domain_name(self) -> str:
-        return self.domain.get_name()
-
-    def _key_modifier(self) -> str:
-        domain_name: str = self.get_domain_name()
-        separator: str = self.get_key_separator()
-        extra_key_mod: str = self._extra_key_modifier()
-        if extra_key_mod != "":
-            return domain_name + separator + extra_key_mod
-        else:
-            return domain_name
-
-    def _extra_key_modifier(self) -> str:
-        return ""
-
-
-class TimePeriod(Composite):
-
-    def test(self):
-        pass
-
-class DtuTimePeriod(TimePeriod):
+    KEY_SEPARATOR: str = "__"
 
     @staticmethod
-    def parse_time_from_key(unparsed_time: str) -> 'DataObject':
-        key: str = unparsed_time
-        if unparsed_time != "":
-            key = "2069"
-        return DataObject.registry.get(key)
-
-class DtuAcademicYear(DtuTimePeriod):
-
-    @staticmethod
-    def extract_year_from_key(unparsed_year: str) -> 'DataObject':
-        key: str = unparsed_year
-        if unparsed_year != "":
-            key = "2069"
-        return DataObject.registry.get(key)
-
-class DtuAcademicTerm(DtuTimePeriod):
+    def serialize_data(data: Tuple[Domain,TimePeriod,str,str]) -> str:
+        domain = data[0].get_name()
+        time = data[1].get_name()
+        class_id = data[2]
+        name = data[3]
+        sep: str = Serializer.KEY_SEPARATOR
+        return domain + sep + time + sep + class_id + sep + name
 
     @staticmethod
-    def extract_year_from_key(unparsed_year: str) -> 'DataObject':
-        key: str = unparsed_year
-        if unparsed_year != "":
-            key = "2069"
-        return DataObject.registry.get(key)
+    def deserialize_key(key: str) -> Tuple[Domain,TimePeriod,str,str]:
+        data_list: List[str] = key.split(Serializer.KEY_SEPARATOR)
+        domain: Domain = Domain(data_list[0])
+        time: TimePeriod = TimePeriod(data_list[1])
+        class_id: str = data_list[2]
+        name: str = data_list[3]
+        return (domain, time, class_id, name)
 
+    @staticmethod
+    def deserialize_domain(key: str) -> Domain:
+        return Serializer.deserialize_key(key)[0]
 
-class TimeSpecificObject(DomainSpecificObject):
+    @staticmethod
+    def deserialize_time_period(key: str) -> TimePeriod:
+        return Serializer.deserialize_key(key)[1]
 
-    def __init__(self, name: str) -> None:
-        self.time_period: TimePeriod
-        super().__init__(name)
+    @staticmethod
+    def deserialize_class_id(key: str) -> str:
+        return Serializer.deserialize_key(key)[2]
 
-    def get_time_period_name(self) -> str:
-        return self.time_period.get_name()
-
-    def _extra_key_modifier(self) -> str:
-        return self.get_time_period_name()
+    @staticmethod
+    def deserialize_name(key: str) -> str:
+        return Serializer.deserialize_key(key)[3]
 
 class DataManager:
     def __init__(self) -> None:
+        self.serializer: Serializer
         self._memory: DAO = Registry()
-        self._disk: DAO = FileAccess()
         self._construction_strategy = ()
 
-    def get(self, key: str) -> DataObject:
-        data_object: DataObject
+    def get(self, key: str) -> 'Composite':
         if self._memory.exists(key):
             data_object = self._memory.get(key)
-        elif self._disk.exists(key):
-            data_object = self._disk.get(key)
         else:
-            pass
+            domain: Domain = self.serializer.deserialize_domain(key)
+            time_period: TimePeriod = self.serializer.deserialize_time_period(key)
+            class_id: str = self.serializer.deserialize_class_id(key)
+            name: str = self.serializer.deserialize_name(key)
+            strategy: Type[DataStrategy] = DataStrategy
+            strategy = domain.get_data_strategy(class_id)
+            data_object = strategy.generate_data_object(time_period, name)
+            self.register(key, data_object)
         return data_object
 
-    def set(self):
+    def register(self, key: str, data_object: 'Composite') -> None:
+        self._memory.set_unique_key(key, data_object)
+
+    def serialize_data(self, data: Tuple[Domain,TimePeriod,str,str]):
+        return self.serializer.serialize_data(data)
+
+class Composite:
+
+    CLASS_ID: str = ""
+    DEFAULT_DATA_MANAGER: DataManager = DataManager()
+
+    def __init__(self, name: str, domain: Domain, time_period: TimePeriod) -> None:
+        self.name: str = name
+        self.domain: Domain = domain
+        self.time_period: TimePeriod = time_period
+        self.class_id: str = self.get_class_id()
+        self.data_manager: DataManager = Composite.DEFAULT_DATA_MANAGER
+
+    def serialize(self, name: str) -> str:
+        domain: Domain = self.domain
+        time: TimePeriod = self.time_period
+        class_id: str = self.class_id
+        return self.data_manager.serialize_data((domain, time, class_id, name))
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_class_id(self) -> str:
+        return self.CLASS_ID
+
+    @abstractmethod
+    def perform_action(self, action: Callable[['Composite'], float]) -> float:
         pass
 
-class DataPoint(TimeSpecificObject):
+    @abstractmethod
+    def build(self) -> None:
+        pass
 
-    def __init__(self, name: str) -> None:
-        self.data_point: TimePeriod
-        super().__init__(name)
+class Container(Composite):
+    def __init__(self, name: str, domain: Domain, time_period: TimePeriod) -> None:
+        self.children: List[Composite]
+        super().__init__(name, domain, time_period)
 
-class School(DomainSpecificObject):
+    def perform_action(self, action: Callable[[Composite], float]) -> float:
+        child_sum: float = 0.0
+        for child in self._get_children():
+            child_sum += child.perform_action(action)
+        return child_sum / len(self._get_children())
+
+    def build(self) -> None:
+        child_names: List[str] = self._get_children_to_be_created()
+        for name in child_names:
+            key: str = self.serialize(name)
+            child: Composite = self.data_manager.get(key)
+            self._add_child(child)
+
+    def _add_child(self, child: 'Composite') -> None:
+        self.children.append(child)
+
+    def _get_children(self) -> List['Composite']:
+        return self.children
+
+    @abstractmethod
+    def _get_children_to_be_created(self) -> List[str]:
+        return []
+
+class EndPoint(Composite):
+    def __init__(self, name: str, domain: Domain, time_period: TimePeriod) -> None:
+        self.data_instance: Composite
+        super().__init__(name, domain, time_period)
+
+    def perform_action(self, action: Callable[[Composite], float]) -> float:
+        data_instance: Composite = self._get_data_instance()
+        average = action(data_instance)
+        return average
+
+    def build(self) -> None:
+        name: str = self.get_name()
+        key: str = self.serialize(name)
+        self.data_instance = self.data_manager.get(key)
+
+    def _get_data_instance(self) -> Composite:
+        return self.data_instance
+
+"""
+class School(Composite):
     def get_data_strategy(self) -> DataStrategy:
         return self.domain.get_data_strategy().school_strategy()
-class Year(TimeSpecificObject):
+class Year(Composite):
     def get_data_strategy(self) -> DataStrategy:
         return self.domain.get_data_strategy().time_strategy()
-class Course(TimeSpecificObject):
+class Course(Composite):
     def get_data_strategy(self) -> DataStrategy:
         return self.domain.get_data_strategy().course_strategy()
-class Term(DomainSpecificObject):
+class Term(Composite):
     def get_data_strategy(self) -> DataStrategy:
         return self.domain.get_data_strategy().time_strategy()
-class Teacher(TimeSpecificObject):
+class Teacher(Composite):
     def get_data_strategy(self) -> DataStrategy:
         return self.domain.get_data_strategy().teacher_strategy()
-class StudyLine(TimeSpecificObject):
+class StudyLine(Composite):
     def get_data_strategy(self) -> DataStrategy:
         return self.domain.get_data_strategy().study_line_strategy()
-class Evaluation(TimeSpecificObject):
+class Evaluation(EndPoint):
     def get_data_strategy(self) -> DataStrategy:
         return self.domain.get_data_strategy().evaluation_strategy()
-class GradeSheet(TimeSpecificObject):
+class GradeSheet(EndPoint):
     def get_data_strategy(self) -> DataStrategy:
         return self.domain.get_data_strategy().grade_sheet_strategy()
-class InfoPage(TimeSpecificObject):
+class InfoPage(EndPoint):
     def get_data_strategy(self) -> DataStrategy:
         return self.domain.get_data_strategy().info_page_strategy()
+"""
 
 #School
 #Year
